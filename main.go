98690723d4c874041e649aba04d646bede5bb8b6
codedsprit <roshan0x01@gmail.com>
2024-06-20 12:21:55 +0545

    chore(main): added Refactor CLI and improve help message.

--- main.go
+++ main.go
@@ -2,24 +2,50 @@ package main
 
 import (
 	"earlymoon/internal/dns"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 )
 
-func main() {
-	if len(os.Args) < 3 {
-		log.Fatalf("Usage: %s <domain> <type1> [<type2> ...]", os.Args[0])
-	}
-	domain := os.Args[1]
-	recordTypes := os.Args[2:]
+func printUsage() {
+	fmt.Fprintf(os.Stderr, "Usage:\n")
+	fmt.Fprintf(os.Stderr, "  %s -d <domain> -t <type>\n\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "Options:\n")
+	fmt.Fprintf(os.Stderr, "  -d, -domain <domain>   The domain to query (required)\n")
+	fmt.Fprintf(os.Stderr, "  -t, -type <type>       The type of DNS record to query (required)\n")
+	fmt.Fprintf(os.Stderr, "  -h                     Show this help message\n")
+	fmt.Fprintf(os.Stderr, "\nExample:\n")
+	fmt.Fprintf(os.Stderr, "  %s -d example.com -t A\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\n")
+}
 
-	for _, recordType := range recordTypes {
-		response, err := dns.Query(domain, recordType)
-		if err != nil {
-			log.Printf("Failed to query DNS for %s: %v", recordType, err)
-			continue
-		}
-		fmt.Printf("Record Type: %s\n%s\n", recordType, response)
+func main() {
+	var domain string
+	var recordType string
+
+	flag.StringVar(&domain, "domain", "", "The domain to query (required)")
+	flag.StringVar(&domain, "d", "", "The domain to query (required)")
+	flag.StringVar(&recordType, "type", "", "The type of DNS record to query (required)")
+	flag.StringVar(&recordType, "t", "", "The type of DNS record to query (required)")
+
+	flag.Usage = printUsage
+	flag.Parse()
+
+	if domain == "" || recordType == "" {
+		flag.Usage()
+		log.Fatalf("Both domain and type are required")
 	}
+
+	response, err := dns.Query(domain, recordType)
+	if err != nil {
+		log.Fatalf("Failed to query DNS: %v", err)
+	}
+
+	if len(response) == 0 {
+		fmt.Printf("No %s records found for %s\n", recordType, domain)
+		return
+	}
+
+	fmt.Println(response)
 }
